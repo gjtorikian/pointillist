dc62237d5d6d1d34d4e8fd1f8bb7039c7a72dd08
Garen Torikian <gjtorikian@gmail.com>
2014-05-14 22:37:49 -0700

    Add basic tests

---
+++ test/pointillist/basic_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+class Pointillist::BasicTest < Test::Unit::TestCase
+
+  def test_it_has_a_version
+    assert Pointillist::VERSION
+  end
+
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,3 @@
+require 'bundler/setup'
+require 'pointillist'
+require 'test/unit'
