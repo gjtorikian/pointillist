a04b567567592bbc330a0804c84ba6d5ad6c6d0b
Garen Torikian <gjtorikian@gmail.com>
2014-05-16 00:41:31 -0700

    Add a bunch of fixtures tests

---
+++ test/pointillist/fixtures_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class Pointillist::FixturesTest < Test::Unit::TestCase
+
+  RUBY_CODE = "#!/usr/bin/ruby\nputs 'foo'"
+  RUBY_CODE_TRAILING_NEWLINE = "#!/usr/bin/ruby\nputs 'foo'\n"
+  REDIS_CODE = File.read(File.join(File.dirname(__FILE__), 'fixtures', 'test_data.c'))
+  COFEESCRIPT_CODE = File.read(File.join(File.dirname(__FILE__), 'fixtures', 'test_data.coffee'))
+  RUBY_CODE = File.read(File.join(File.dirname(__FILE__), 'fixtures', 'test_data.ruby'))
+
+  def test_highlight_defaults_to_html
+    code = Pointillist.highlight(RUBY_CODE, "Ruby")
+    assert_match '<span class="c1">#!/usr/bin/ruby</span>', code
+    assert_equal '<div class', code[0..9]
+  end
+
+end
