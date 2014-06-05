dbdc92a6ddc6b9fc7483891526d46a349ab68a32
Garen Torikian <gjtorikian@gmail.com>
2014-06-04 22:30:24 -0700

    Fix a typo

--- test/pointillist/fixtures_test.rb
+++ test/pointillist/fixtures_test.rb
@@ -6,7 +6,7 @@ class Pointillist::FixturesTest < Test::Unit::TestCase
   RUBY_CODE_TRAILING_NEWLINE = "#!/usr/bin/ruby\nputs 'foo'\n"
   REDIS_CODE = File.read(File.join(File.dirname(__FILE__), 'fixtures', 'test_data.c'))
   COFEESCRIPT_CODE = File.read(File.join(File.dirname(__FILE__), 'fixtures', 'test_data.coffee'))
-  RUBY_CODE = File.read(File.join(File.dirname(__FILE__), 'fixtures', 'test_data.ruby'))
+  RUBY_CODE = File.read(File.join(File.dirname(__FILE__), 'fixtures', 'test_data.rb'))
 
   def test_highlight_defaults_to_html
     code = Pointillist.highlight(RUBY_CODE, "Ruby")
